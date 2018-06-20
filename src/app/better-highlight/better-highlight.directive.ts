f7555cbc53f1732239b8aeb177decb4aa1718787
AngelCobian <angelcobian@outlook.com>
2018-06-20 15:08:31 -0700

    using @hostbinding to color

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, Renderer2, ElementRef, OnInit, HostListener } from '@angular/core';
+import { Directive, Renderer2, ElementRef, OnInit, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[BetterHighlight]'
@@ -8,17 +8,29 @@ export class BetterHighlightDirective implements OnInit {
   constructor(private renderer: Renderer2,
   private elementRef: ElementRef) { }
 
-  ngOnInit(){
-    // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
-    // Fourth argument is optional, We can set !importent in that argument
+  ngOnInit() {
+  //   // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
+  //   // Fourth argument is optional, We can set !importent in that argument
   }
+    /* host listener exmaple */
+  //   @HostListener('mouseenter') mouseEnter(){ // Hostlistener decorator is added to a method we want to execute on event
+  //     // listening for mouseenter event on host
+  //     this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
+  //   }
+  //   @HostListener('mouseleave') mouseOut(){
+  //     // listening for mouseleave event on host
+  //     this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'gray');
+  //   }
+  // tslint:disable-next-line:member-ordering
+  @HostBinding('style.backgroundColor') backgroundColor: string = 'transparent';
+    // bind to host property using Hostbinding
+    // defind in string
+    // camelCase property name because we are accessing DOM property not CSS
 
-    @HostListener('mouseenter') mouseEnter(){ // Hostlistener decorator is added to a method we want to execute on event
-      // listening for mouseenter event on host
-      this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
+    @HostListener('mouseenter') mouseEnter(){
+    this.backgroundColor = 'blue';
     }
     @HostListener('mouseleave') mouseOut(){
-      // listening for mouseleave event on host
-      this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'gray');
-    }
+this.backgroundColor = 'transparent';
+        }
 }
