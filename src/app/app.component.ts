6d57fe1b704244144b8828aeacaf8c8a963fda4f
AngelCobian <angelcobian@outlook.com>
2018-06-20 17:39:27 -0700

    understanding *Switch & ngSwitch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,33 @@ export class AppComponent {
   onlyOdd = false;
   oddNumbers = [1, 3 , 5];
   evenNumbers = [2, 4];
+  value: number = 10;
+
+
+
+  ngOnInit(){
+    // if (this.value == 5 ){
+    //   console.log('inside first if');
+    // } else if(this.value == 10){
+    //   console.log('inside second if');
+    // } else if(this.value ==100){
+    //   console.log("idjc");
+    // }else{
+    //   console.log('none');
+    // }
+  // }
+  switch(this.value){
+    case 5:
+      console.log('case 5');
+      break;
+    case 10:
+      console.log('case 10');
+      break;
+    case 100:
+      console.log('case 100');
+      break;
+    default:
+      console.log('none');
+      break;
+  }
 }
